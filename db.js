8bc0567c50d9f0aeb8139974bb5e2fad7f8bbe9a
AdriSaurXYZ <adrianaguilo@paucasesnovescifp.cat>
2025-05-21 11:11:10 +0200

    log db

--- db.js
+++ db.js
@@ -12,6 +12,15 @@ const db = mysql.createConnection({
     }
 });
 
+console.log("DB config:", {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+
 db.connect((err) => {
     if (err) {
         console.error('❌ Error al conectar a la base de datos:', err);
