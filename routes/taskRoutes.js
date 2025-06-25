b7951edc2d8e53c32df9786d09b05fa8e83dc794
AdriSaurXYZ <adrianaguilo@paucasesnovescifp.cat>
2025-06-25 13:21:08 +0200

    updateprofile

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
-const { authenticate } = require('../middleware/authMiddleware');
+const authenticate = require('../middleware/authMiddleware');  // <--- cambio aquí
 const multer = require('multer');
-const { upload } = require('../config/cloudinary'); // usa el upload de Cloudinary
-
-// Configuración de multer
-//const upload = multer({ dest: 'uploads/' });
+const { upload } = require('../config/cloudinary');
 
 // Rutas con autenticación
 router.get('/', authenticate, taskController.getTasks);
@@ -15,6 +12,5 @@ router.put('/:id', authenticate, taskController.updateTask);
 router.delete('/:id', authenticate, taskController.deleteTask);
 router.put('/:id/category', authenticate, taskController.updateTaskCategory);
 router.post('/', authenticate, upload.single('image'), taskController.createTask);
-
 
 module.exports = router;
